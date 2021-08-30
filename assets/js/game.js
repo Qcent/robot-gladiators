c7e97553e1c82a3a3267ed2f9bf7147c7c534ee0
Dave Quinn <dquinn8@cogeco.ca>
2021-08-30 18:47:04 -0400

    robots now battle one round and checks for deaths

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,42 @@
-window.alert("this is an alert! the JacaScript engine is running!!!");
+function fight() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators! \n" +
+        "The Fight is Begin, " + playerName);
+
+    // Subtract the value of `playerAttack` from value of `enemyHealth` vaiable and use the result to update the `enemyHealth`variable.
+    enemyHealth = enemyHealth - playerAttack;
+
+    // Log a resulting message to the console so we know that it worked.
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update teh value in the `playerHealth` variable.
+    playerHealth = playerHealth - enemyAttack;
+
+    // Log a resulting message to the consoe so we know it worked.
+    console.log(enemyName + " has attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    //check enemys health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    //check players health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+}
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+fight();
